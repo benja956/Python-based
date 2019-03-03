542c54a0abc085961cbb58bf1dd2ad5bf84de7b5
bool <34473926+benja956@users.noreply.github.com>
2019-03-03 20:03:43 +0800

    Create 三天打鱼.py

---
+++ 三天打鱼.py
@@ -0,0 +1,16 @@
+#santiandayu.py
+dayup = 1
+for i in range(365):
+    if i % 5 in [4,5]:
+       dayup = dayup * 0.99
+    else:
+         dayup = dayup * 1.01
+print("三天打鱼两天晒网只有：{:.3f}".format(dayup))
+
+ddup = 1
+for i in range(365):
+    if i % 7 in [6,0] :
+        ddup = ddup * 0.99
+    else:
+        ddup = ddup * 1.02
+print("多一份努力的收获是：{:.3f}".format(ddup))	   
