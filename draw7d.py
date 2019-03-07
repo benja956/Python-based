2e0682d9c084301b42686982a051dcf716f87cee
bool <34473926+benja956@users.noreply.github.com>
2019-03-07 22:38:06 +0800

    Update draw7d.py

--- draw7d.py
+++ draw7d.py
@@ -31,6 +31,7 @@ main()
 """
 #V2
 
+"""
 import turtle as tt,time
 def drawgap(n):
      tt.penup()
@@ -79,4 +80,57 @@ def main():
      drawdate(time.strftime("%Yn%my%dr",time.gmtime()))
      tt.hideturtle()
      tt.done()
+main()
+"""
+#v3
+import time
+import turtle as tt
+inp = eval(input("请输入时间："))
+
+def drawline(draw):
+     tt.pendown() if draw else tt.penup()
+     tt.fd(40)
+     tt.right(90)
+
+def drawdigit(digit):
+     drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
+     drawline(True) if digit in [0,1,3,4,5,6,7,8,9] else drawline(False)
+     drawline(True) if digit in [0,2,3,5,6,8,9] else drawline(False)
+     drawline(True) if digit in [0,2,6,8] else drawline(False)
+     tt.left(90)
+     drawline(True) if digit in [0,4,5,6,8,9] else drawline(False)
+     drawline(True) if digit in [0,2,3,5,6,7,8,9] else drawline(False)
+     drawline(True) if digit in [0,1,2,3,4,7,8,9] else drawline(False)
+     tt.left(180)
+     tt.penup()
+     tt.fd(25)
+
+def drawdate(date):
+     for i in date:
+          drawdigit(eval(i))     
+
+def drawdate(date):
+     tt.pencolor("red")
+     for i in date:
+          if i == "s":
+               #draw
+               tt.write("时",font=("Arial", 18, "normal"))
+               tt.pencolor("green")
+               drawgap(40)
+          elif i == "m":
+               #draw
+               tt.write("分",font=("Arial", 18, "normal"))
+               tt.pencolor("blue")
+               drawgap(40)
+          elif i == "r":
+               #draw
+               tt.write("秒",font=("Arial", 18, "normal"))
+               drawgap(40)
+          else:
+               drawdigit(eval(i))
+
+def main():
+     drawdate(time.strftime("%Yn%my%dr",time.gmtime())
+
+
 main()
