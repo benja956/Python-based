734726e62f0aabb522432bcf0b0d235038e58be7
bool <34473926+benja956@users.noreply.github.com>
2019-03-16 22:55:15 +0800

    Create window.py

---
+++ window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("聊天词云生成器v1.0")
+window.geometry("200*100")
+
+l = tk.Label(window,text="",bg="pink",width=15,height=2)
+l.pack()
+
+window.mainloop()
